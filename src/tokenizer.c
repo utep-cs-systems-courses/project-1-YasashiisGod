6a42fff0d3d9abe55f0b8ecd99cdb37145024742
YasashiisGod <secretlyytheflash@gmail.com>
2020-06-27 20:31:44 -0600

    Added non_space_character

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include "tokenizer.h"
+//#include "history.h"
 
 int main ()
 {
-  puts("hello world");
-
+  char f = 'b';
+  
   return 0;
+}
+
+
+
+int space_char(char c)
+{
+    if (c == ' ' || c == '\t')
+    {
+      return 1;
+    }
+  else
+    {
+      return 0;
+    }
+}
+
+int non_space_char(char c)
+{
+  if (space_char(c) == 0)
+    {
+      return 1;
+    }
+  else
+    {
+      return 0;
+    }
 }
 
