8aba8920093f63438869defcf92d4786ed264c61
YasashiisGod <secretlyytheflash@gmail.com>
2020-06-29 13:04:55 -0600

    Initialized history linked list

--- src/history.c
+++ src/history.c
@@ -2,3 +2,15 @@
 #include <stdlib.h>
 #include "history.h"
 
+List* init_history()
+{
+  List *history = (List*)malloc(sizeof(List*));
+  history->root = NULL;
+  return history;
+}
+
+void add_history(List *list, char *str)
+{
+  int len = len
+}
+
