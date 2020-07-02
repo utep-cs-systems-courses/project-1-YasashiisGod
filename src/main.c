3a1c0fb12939ebadc0b51be7e687a31a0e2686cf
YasashiisGod <secretlyytheflash@gmail.com>
2020-07-01 21:57:28 -0600

    Tokenizer UI functional, history UI unachieved

--- src/main.c
+++ src/main.c
@@ -3,10 +3,12 @@
 #include "tokenizer.h"
 #include "history.h"
 
+
 int main ()
 {
   char hello[] = "Hello world";
   char *str = hello;
+  int id_tracker = 1;
 
   char array [30];
   while (1)
@@ -18,11 +20,42 @@ int main ()
       printf("->");
       fgets(array, 29, stdin);
       char *str = &array[0];
+
+      if (*str == 'q') // quit
+	{
+	  exit(0);
+	}
+
       char** tokenizer = tokenize(str);
       print_tokens(tokenizer);
-      
-      free_tokens(tokenizer);
 
+      List *list = init_history();
+
+      //      char string[29] = *str;
+      
+      /*if (*str == "!1")
+	{
+	  print_tokens(tokenize(get_history(list,1)));
+	}
+      if (strcmp(*str,"!2"))
+	{
+	  print_tokens(tokenize(get_history(list,2)));
+	}
+      if (*str == "!3")
+	{
+	  print_tokens(tokenize(get_history(list,3)));
+	}
+      if (*str == "!4")
+	{
+	}
+      */
+      
+      add_history(list, str);
+
+      free_tokens(tokenizer);
+      free_history(list);
+      id_tracker++;
+      get_history(list,1);
       
     }
 }
